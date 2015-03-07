98ad42c151e055ec16e629bbca1755379b777ba8
temeyun <poonkeegan@gmail.com>
2015-03-07 11:21:12 -0500

    Fixed package naming

--- src/eulerQuestions/Palindrome.java
+++ src/eulerQuestions/Palindrome.java
@@ -1,6 +1,6 @@
-package Palindrome;
+package eulerQuestions;
 
-public class Main {
+public class Palindrome {
 
 	public static void main(String[] args) {
 		int greatest = 0;
---
+++ src/eulerQuestions/Sum_Square_Diff.java
@@ -0,0 +1,10 @@
+package eulerQuestions;
+
+public class Sum_Square_Diff {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

f89dbbeef63d7d09daf12f8d5337d24bc64cd9bc
temeyun <poonkeegan@gmail.com>
2015-03-07 11:28:26 -0500

    Completed Q6
    i^2 != i*i

--- src/eulerQuestions/Sum_Square_Diff.java
+++ src/eulerQuestions/Sum_Square_Diff.java
@@ -3,8 +3,14 @@ package eulerQuestions;
 public class Sum_Square_Diff {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int sumOfSq = 0;
+		int sqOfSum = 0;
+		for(int i = 1; i <= 100; i++){
+			sumOfSq += i*i;
+			sqOfSum += i;
+		}
+		sqOfSum *= sqOfSum;
+		System.out.println(sqOfSum - sumOfSq);
 	}
 
 }
